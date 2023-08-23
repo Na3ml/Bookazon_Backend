098738c4d47c71713cd94e17311c5169b5b4cee3
unknown <mohamedsabaawy@hotmail.com>
2023-08-24 01:11:52 +0300

    bookin endpoint

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,10 +9,7 @@ class Order extends Model
 
     protected $table = 'orders';
     public $timestamps = true;
-<<<<<<< HEAD
     protected $fillable = array('user_id','room_id', 'order_no', 'booking_date', 'check_in_date', 'check_out_date', 'transaction_id', 'paid_amount', 'status', 'payment_method');
-=======
-    protected $fillable = array('user_id', 'order_no', 'booking_date', 'check_in_date', 'check_out_date', 'transaction_id', 'paid_amount', 'status', 'payment_method');
->>>>>>> 7338c63cfd5605463560cc793d1d16ca2af4f423
+
 
 }
