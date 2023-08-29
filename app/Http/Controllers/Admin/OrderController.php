9eb7c12b7d00c2433fd4853c23f3bd6e61d0a11e
belal <belal.naeem@yahoo.com>
2023-08-29 19:32:43 +0300

    test

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -8,95 +8,97 @@ use Illuminate\Http\Request;
 use App\Models\Order;
 use Illuminate\Support\Facades\Auth;
 
-class OrderController extends Controller
-{
+class OrderController extends Controller {
 
-  /**
-   * Display a listing of the resource.
-   *
-   * @return Response
-   */
-  public function index()
-  {
+    /**
+    * Display a listing of the resource.
+    *
+    * @return Response
+    */
+
+    public function index() {
         $owner = Auth::user();
+        $orders = [];
         $owner_properties = $owner->properties;
-        foreach ($owner_properties as $owner_property){
-            $rooms = $owner_property->rooms;
-            foreach ($rooms as $room){
-                $orders = $room->orders;
+        if ( count( $owner_properties ) ) {
+            foreach ( $owner_properties as $owner_property ) {
+                $rooms = $owner_property->rooms;
+                foreach ( $rooms as $room ) {
+                    $orders = $room->orders;
+                }
             }
         }
 
-//        dd($orders);
+        //        dd( $orders );
 
-      //
-      return view( 'admin.order.index' ,compact('orders'));
+        //
+        return view( 'admin.order.index', compact( 'orders' ) );
 
-  }
+    }
 
-  /**
-   * Show the form for creating a new resource.
-   *
-   * @return Response
-   */
-  public function create()
-  {
+    /**
+    * Show the form for creating a new resource.
+    *
+    * @return Response
+    */
 
-  }
+    public function create() {
 
-  /**
-   * Store a newly created resource in storage.
-   *
-   * @return Response
-   */
-  public function store(Request $request)
-  {
+    }
 
-  }
+    /**
+    * Store a newly created resource in storage.
+    *
+    * @return Response
+    */
 
-  /**
-   * Display the specified resource.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function show($id)
-  {
+    public function store( Request $request ) {
 
-  }
+    }
 
-  /**
-   * Show the form for editing the specified resource.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function edit($id)
-  {
+    /**
+    * Display the specified resource.
+    *
+    * @param  int  $id
+    * @return Response
+    */
 
-  }
+    public function show( $id ) {
 
-  /**
-   * Update the specified resource in storage.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function update($id)
-  {
+    }
 
-  }
+    /**
+    * Show the form for editing the specified resource.
+    *
+    * @param  int  $id
+    * @return Response
+    */
 
-  /**
-   * Remove the specified resource from storage.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function destroy($id)
-  {
+    public function edit( $id ) {
 
-  }
+    }
+
+    /**
+    * Update the specified resource in storage.
+    *
+    * @param  int  $id
+    * @return Response
+    */
+
+    public function update( $id ) {
+
+    }
+
+    /**
+    * Remove the specified resource from storage.
+    *
+    * @param  int  $id
+    * @return Response
+    */
+
+    public function destroy( $id ) {
+
+    }
 
 }
 
